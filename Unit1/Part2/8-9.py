16e1104c897c586ddad930d24d88674982cdc2c9
Himanshu golash <49239676+golashboy@users.noreply.github.com>
2020-01-17 00:25:55 +0530

    Update 8-9.py

--- Unit1/Part2/8-9.py
+++ Unit1/Part2/8-9.py
@@ -1,1 +1,17 @@
-# question 8 and 9 DO it on your own.
+# Use of break statement 
+
+for m in "himan":
+    if m == "i":
+        break
+    print(m)
+
+print("easy stuff!")
+
+# Program to show the use of continue statement
+
+for v in "himan":
+    if v == "i":
+        continue
+    print(v)
+
+print("very easy stuff!")
